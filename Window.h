1dd332e2386f362c9f122bd72efaef2542f33aa2
Angel Pineda <90359523+angelpineda-7@users.noreply.github.com>
2025-04-12 21:05:06 -0600

    Documentos práctica 08

---
+++ Window.h
@@ -0,0 +1,50 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	GLfloat getrotay() { return rotay; }
+	GLfloat getrotax() { return rotax; }
+	GLfloat getrotaz() { return rotaz; }
+	GLfloat avanzar() { return adelante; }
+	GLfloat retroceder() { return atras; }
+	GLfloat helociptero() { return helicopter; }
+	GLfloat llantas_Adelante() { return sigue; }
+	GLfloat llantas_Atras() { return retrocede; }
+	GLfloat abrir() { return cofre; }
+	GLint luz() { return prendido; }
+	GLint luzArbol() { return arbol; }
+	GLint avanzando() { return isAvanzando; }
+
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	GLfloat rotax,rotay,rotaz, adelante, atras, cofre, sigue, retrocede, helicopter;
+	GLint c, prendido, arbol, luzc, luza, isAvanzando;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	bool mouseFirstMoved;
+	void createCallbacks();
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+};
+
